2d639375f8de76d644ecc9dbe7a89ff219f7fcd8
Sarbik Betal <sarbikb55@gmail.com>
2019-08-25 01:12:39 +0530

    Express routes modularized

--- app.js
+++ app.js
@@ -3,54 +3,27 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-require('./models/Schemas');
-const Model = mongoose.model('record');
 
+// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI,
     { useNewUrlParser: true })
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
-
+// Express.js Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.post('/post', (req, res) => {
-    new Model(req.body).save()
-        .then(() => {
-            res.json({ msg: "Record added successfully" });
-        })
-        .catch((err) => {
-            res.json({ msg: err });
-        });
-    // res.json(req.body);
-});
+// Express.js Routes
+const user = require("./routes/user");
+const info = require("./routes/info");
 
-app.put('/update', (req, res) => {
-    Model.updateOne({ "location": req.body.location }, { $push: { "info": req.body.info } }, (err) => {
-        if (err)
-            res.json({ msg: err });
-        else
-            res.json({ msg: "Record updated successfully" });
-    });
-});
-
-app.get('/search', (req, res) => {
-    res.set({ 'Access-Control-Allow-Origin': '*/*' });
-        
-    Model.find({ 'location': { $regex: req.query.words, $options: 'gi' } },
-        (err, result) => {
-            if (err)
-                res.json({ msg: err });
-            else
-                res.json(result);
-        });
-});
+app.use('/user', user);
+app.use('/info', info);
 
 
 //Server Init
 const port = process.env.PORT || 5000;
-
 const server = app.listen(port, () => {
     console.log(`Server started on port ${server.address().port}`);
 });
---
+++ routes/info.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+// Load the schema for MongoDB records
+require('../models/Schemas');
+const Model = mongoose.model('record');
+
+
+///////////////////////////////////// MongoDB routes  //////////////////////////////////////////////
+router.post('/mongo/post', (req, res) => {
+    new Model(req.body).save()
+        .then(() => {
+            res.json({ msg: "Record added successfully" });
+        })
+        .catch((err) => {
+            res.json({ msg: err });
+        });
+});
+
+router.put('/mongo/update', (req, res) => {
+    Model.updateOne({ "location": req.body.location }, { $push: { "info": req.body.info } }, (err) => {
+        if (err)
+            res.json({ msg: err });
+        else
+            res.json({ msg: "Record updated successfully" });
+    });
+});
+
+router.get('/mongo/search', (req, res) => {
+    res.set({ 'Access-Control-Allow-Origin': '*/*' });
+        
+    Model.find({ 'location': { $regex: req.query.words, $options: 'gi' } },
+        (err, result) => {
+            if (err)
+                res.json({ msg: err });
+            else
+                res.json(result);
+        });
+});
+
+///////////////////////////////////// MongoDB routes end //////////////////////////////////////////////
