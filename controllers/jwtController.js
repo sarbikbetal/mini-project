48a088118994f53093480c6ae9a2bfec8e5918ef
Sarbik Betal <sarbikb55@gmail.com>
2019-10-31 21:30:05 +0530

    JWT Authentication added

---
+++ controllers/jwtController.js
@@ -0,0 +1,41 @@
+const jwt = require('jsonwebtoken');
+
+// JWT Secret
+const jwtKey = process.env.SECRET;
+const jwtExpiry = "5d";
+
+///////////////////////////// JWT Generation and Validation //////////////////////////////////////
+
+let JWTgen = (user) => {
+  return new Promise((resolve, reject) => {
+    jwt.sign({ licence: user.licence }, jwtKey, {
+      algorithm: 'HS256',
+      expiresIn: jwtExpiry
+    }, (err, token) => {
+      if (err)
+        reject(err)
+      else
+        resolve(token);
+    });
+  });
+}
+
+let JWTcheck = (token) => {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, jwtKey,(err, payload) => {
+      if (err)
+        reject(err)
+      else
+        resolve(payload.licence);
+    });
+  });
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Exports //
+module.exports = {
+  JWTgen: JWTgen,
+  JWTcheck: JWTcheck
+}
